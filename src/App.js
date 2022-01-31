729b38142731a5ba61df9452bc3bd1d6d0b1dd55
Jaemin Ha <woals3527@naver.com>
2022-01-31 14:54:55 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let [counter, setCounter] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Total clicks: {counter}</h3>
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >
+        Click me
+      </button>
     </div>
   );
 }
