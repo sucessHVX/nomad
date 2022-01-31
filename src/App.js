f3d9711e033e6551be4d5191ee43c51f324054e6
Jaemin Ha <woals3527@naver.com>
2022-01-31 16:51:16 +0900

    flip commit

--- src/App.js
+++ src/App.js
@@ -3,18 +3,38 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let [counter, setCounter] = useState(0);
+  let [minutes, setMinutes] = useState();
+  let [flipped, setFlipped] = useState(false);
 
+  const reset = () => setMinutes(0);
+  const onChange = (event) => setMinutes(event.target.value);
+  const onFlip = () => {
+    reset();
+    setFlipped((current) => !current);
+  };
   return (
     <div>
-      <h3>Total clicks: {counter}</h3>
-      <button
-        onClick={() => {
-          setCounter(counter + 1);
-        }}
-      >
-        Click me
-      </button>
+      <h1>Super Converter</h1>
+      <label htmlFor="minutes">Minutes</label>
+      <input
+        value={flipped ? minutes * 60 : minutes}
+        id="minutes"
+        placehoder="Minutes"
+        type="number"
+        onChange={onChange}
+        disabled={flipped}
+      />
+      <label htmlFor="hours">Hours</label>
+      <input
+        value={flipped ? minutes : minutes / 60}
+        id="hours"
+        placehoder="Hours"
+        type="number"
+        disabled={!flipped}
+        onChange={onChange}
+      />
+      <button onClick={reset}>Reset</button>
+      <button onClick={onFlip}>Flip</button>
     </div>
   );
 }
